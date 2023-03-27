ab2d8e3944dc9ceec00f17fc15d4a69707f3ad18
Turner <63553505+PC1up@users.noreply.github.com>
2023-03-27 21:39:22 +0200

    Add option to disable tNotify; Formatting stuff

--- M-Teleport/server.lua
+++ M-Teleport/server.lua
@@ -4,41 +4,85 @@
 ---     https://discord.gg/cPSAuJppyr    ---
 --- Date Created: 03/18/2023 Time: 21:00 ---
 --------------------------------------------
+local tNotify = Config.tNotify
 
 if Config.ScriptEnabled then
 if GetCurrentResourceName() == "M-Teleport" then
 
 RegisterCommand(Config.Teleport.TPToCmd, function(source, args, raw)
     if IsPlayerAceAllowed(source, "furiousfoxgg.tpto") then 
-    if args[1] then 
-        local SrcPed = source 
-        local UserCoords = GetEntityCoords(GetPlayerPed(args[1]))
-        SetEntityCoords(GetPlayerPed(SrcPed), UserCoords.x, UserCoords.y, UserCoords.z + 0.5, 0, 0, 0, false)
-        TriggerClientEvent('t-notify:client:Alert', source, {style  =  'error', message  =  'Teleported to '..GetPlayerName(SrcPed):gsub("~%a~", ""):gsub("~italic~", "")})        
-        TriggerClientEvent("M:Teleport:DebugLog", -1, GetPlayerName(source).." Teleported to "..GetPlayerName(SrcPed).."")
-    else
-        TriggerClientEvent('t-notify:client:Alert', source, {style  =  'error', message  =  'Incorrect Usage: \'/' ..Config.Teleport.TPToCmd..' [ID]\''})        
+        if args[1] and tonumber(args[1]) then -- tonumber just checks if first arg is a number lol
+            local SrcPed = source 
+            local target = tonumber(args[1])
+            if not GetPlayerName(target) then -- Check if provided ID is valid (If someone with this ID is online)
+                if tNotify then
+                    TriggerClientEvent('t-notify:client:Alert', source, {style  =  'error', message  =  'You provided an invalid ID.'})
+                    return
+                else
+                    TriggerClientEvent('chat:addMessage', source, {args = {"~r~You provided an invalid ID."}})
+                    return
+                end
+            end
+            local UserCoords = GetEntityCoords(GetPlayerPed(target))
+            SetEntityCoords(GetPlayerPed(SrcPed), UserCoords.x, UserCoords.y, UserCoords.z + 0.5, 0, 0, 0, false)
+            if tNotify then
+                TriggerClientEvent('t-notify:client:Alert', source, {style  =  'error', message  =  'Teleported to '..GetPlayerName(target):gsub("~%a~", ""):gsub("~italic~", "")})        
+            else
+                TriggerClientEvent('chat:addMessage', source, {args = {"~g~Teleported to " .. GetPlayerName(target)}})
+            end
+            TriggerClientEvent("M:Teleport:DebugLog", -1, GetPlayerName(source).." Teleported to "..GetPlayerName(target).."")
+        else
+            if tNotify then
+                TriggerClientEvent('t-notify:client:Alert', source, {style  =  'error', message  =  'Incorrect Usage: \'/' ..Config.Teleport.TPToCmd..' [ID]\''})        
+            else
+                TriggerClientEvent('chat:addMessage', source, {args = {"~r~Incorrect Usage: /" .. Config.Teleport.TPToCmd .. " [ID]"}})
+            end
         end
     else
-        TriggerClientEvent('t-notify:client:Alert', source, {style  =  'error', message  =  'You do not have permission to use this command.'})
+        if tNotify then
+            TriggerClientEvent('t-notify:client:Alert', source, {style  =  'error', message  =  'You do not have permission to use this command.'})
+        else
+            TriggerClientEvent('chat:addMessage', source, {args = {"~r~You do not have permission to use this command."}})
+        end
     end
 end)
 
 RegisterCommand(Config.Teleport.TPBringCmd, function(source, args, raw)
     if IsPlayerAceAllowed(source, "furiousfoxgg.tpbring") then 
-    if args[1] then 
-        local SrcPed = args[1]
-        local StaffCoords = GetEntityCoords(GetPlayerPed(source))
-        TriggerClientEvent('t-notify:client:Alert', source, {style  =  'error', message  =  'Teleported '..GetPlayerName(SrcPed):gsub("~%a~", ""):gsub("~italic~", "").. " To you."})  
-        SetEntityCoords(GetPlayerPed(SrcPed), StaffCoords.x, StaffCoords.y, StaffCoords.z + 0.5, 0, 0, 0, false)
-        TriggerClientEvent("M:Teleport:DebugLog", -1, GetPlayerName(source).." Teleported User: "..GetPlayerName(SrcPed).." to Staff.")
-    else
-        TriggerClientEvent('t-notify:client:Alert', source, {style  =  'error', message  =  'Incorrect Usage: \'/' ..Config.Teleport.TPBringCmd..' [ID]\''})        
+        if args[1] and tonumber(args[1]) then 
+            local SrcPed = tonumber(args[1])
+            if not GetPlayerName(SrcPed) then
+                if tNotify then
+                    TriggerClientEvent('t-notify:client:Alert', source, {style  =  'error', message  =  'You provided an invalid ID.'})
+                    return
+                else
+                    TriggerClientEvent('chat:addMessage', source, {args = {"~r~You provided an invalid ID."}})
+                    return
+                end
+            end
+            local StaffCoords = GetEntityCoords(GetPlayerPed(source))
+            if tNotify then
+                TriggerClientEvent('t-notify:client:Alert', source, {style  =  'error', message  =  'Teleported '..GetPlayerName(SrcPed):gsub("~%a~", ""):gsub("~italic~", "").. " To you."})  
+            else
+                TriggerClientEvent('chat:addMessage', source, {args = {"~g~Teleported " .. GetPlayerName(SrcPed) .. "~g~ to you."}}) -- (Changing color to green after name just incase their name was coloured)
+            end
+            SetEntityCoords(GetPlayerPed(SrcPed), StaffCoords.x, StaffCoords.y, StaffCoords.z + 0.5, 0, 0, 0, false)
+            TriggerClientEvent("M:Teleport:DebugLog", -1, GetPlayerName(source).." Teleported User: "..GetPlayerName(SrcPed).." to Staff.")
+        else
+            if tNotify then
+                TriggerClientEvent('t-notify:client:Alert', source, {style  =  'error', message  =  'Incorrect Usage: \'/' ..Config.Teleport.TPBringCmd..' [ID]\''})        
+            else
+                TriggerClientEvent('chat:addMessage', source, {args = {"~r~Incorrect Usage: /" .. Config.Teleport.TPBringCmd .. " [ID]"}})
+            end
         end
     else
-        TriggerClientEvent('t-notify:client:Alert', source, {style  =  'error', message  =  'You do not have permission to use this command.'})        
+        if tNotify then
+            TriggerClientEvent('t-notify:client:Alert', source, {style  =  'error', message  =  'You do not have permission to use this command.'})
+        else
+            TriggerClientEvent('chat:addMessage', source, {args = {"~r~You do not have permission to use this command."}})
+        end
     end
 end)
 
-end 
+end
 end
